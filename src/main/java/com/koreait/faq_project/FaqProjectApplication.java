1c9780ce719e6002fbcf5b83c90d93bf28383e98
meustar <meustar829@gmail.com>
2024-11-01 16:25:01 +0900

    프로젝트 생성

---
+++ src/main/java/com/koreait/faq_project/FaqProjectApplication.java
@@ -0,0 +1,13 @@
+package com.koreait.faq_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaqProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaqProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/koreait/faq_project/FaqProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreait.faq_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaqProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
